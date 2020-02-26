f27b416534e37c75f0532867a11d9fa276d1265f
mgrady91 <michaelgrady91@gmail.com>
2020-02-26 17:40:59 -0500

    Update Notify.sh

--- Notify/Notify.sh
+++ Notify/Notify.sh
@@ -7,6 +7,7 @@ TOKEN_RAW="/var/tmp/idtokenraw"
 GIVEN_NAME=Archie
 TOKEN_GIVEN_NAME=$(echo "$(cat $TOKEN_BASIC)" | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep given_name)
 TOKEN_UPN=$(echo "$(cat $TOKEN_BASIC)" | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep upn)
+JAMF_BINARY="/usr/local/bin/jamf"
 
 echo "STARTING RUN" >> $NOTIFY_LOG
 
@@ -24,44 +25,44 @@ sleep 10
 
 
 
-#3 - Everything you need for your first day
+#2 - Everything you need for your first day
 
-###Jamf Triggers
 echo "Command: Image: /System/Library/CoreServices/Install in Progress.app/Contents/Resources/Installer.icns" >> $NOTIFY_LOG
 echo "Command: MainTitle: Installing everything you need for your first day" >> $NOTIFY_LOG
 echo "Command: MainText: All of the core apps you'll need are already being installed. Once we're ready to start, you'll find Google Chrome, Slack, and Zoom are all ready to go." >> $NOTIFY_LOG
 sleep 5
 
 #4 - Install Applications
+##Jamf Triggers
 
 echo "Command: Image: /usr/local/bin/SentinelOne.png" >> $NOTIFY_LOG
 echo "Status: Installing SentinelOne..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-S1"
+$JAMF_BINARY policy -event "JC-S1"
 sleep 5
 
 echo "Command: Image: /usr/local/bin/NessusAgent.jpg" >> $NOTIFY_LOG
 echo "Status: Installing Nessus Agent..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-Nessus"
+$JAMF_BINARY policy -event "JC-Nessus"
 sleep 5
 
 echo "Command: Image: /usr/local/bin/Umbrella.icns" >> $NOTIFY_LOG
 echo "Status: Installing Umbrella..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-Umbrella"
+$JAMF_BINARY policy -event "JC-Umbrella"
 sleep 5
 
 echo "Command: Image: /usr/local/bin/Chrome.icns" >> $NOTIFY_LOG
 echo "Status: Installing Google Chrome..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-Chrome"
+$JAMF_BINARY policy -event "JC-Chrome"
 sleep 5
 
 echo "Command: Image: /usr/local/bin/Zoom.icns" >> $NOTIFY_LOG
 echo "Status: Installing Zoom..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-Zoom"
+$JAMF_BINARY policy -event "JC-Zoom"
 sleep 5
 
 echo "Command: Image: /usr/local/bin/slack.icns" >> $NOTIFY_LOG
 echo "Status: Installing Slack..." >> $NOTIFY_LOG
-$JAMF_BINARY -event "JC-Slack"
+$JAMF_BINARY policy -event "JC-Slack"
 sleep 5
 
 # - Finishing up
@@ -73,5 +74,3 @@ sleep 3
 
 echo "Command: Quit" >> $NOTIFY_LOG
 sleep 1
-
-rm -rf $NOTIFY_LOG
