5c7a0493d9d4f54a6dab97bbc60bfad346178337
mgrady91 <michaelgrady91@gmail.com>
2020-02-26 16:04:52 -0500

    Create Postinstall.sh

---
+++ Notify/Postinstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# postinstall script for:
+# Jamf Combo Installer for DEP Prestage
+
+TMP_PATH=/private/tmp
+LOGIN_FILENAME=JamfConnectLoginOkta-1.9.0.pkg
+VERIFY_FILENAME=JamfConnectVerify-x.pkg
+SYNC_FILENAME=JamfConnectSync-1.2.2.pkg
+
+# Install Login
+installer -pkg "$TMP_PATH"/"$LOGIN_FILENAME" -target $3
+
+# if Okta installation, use this command:
+installer -pkg "$TMP_PATH"/"$SYNC_FILENAME" -target $3
+
+# FANCY VERSION: Okta with EULA and post login script with Notify mechanism
+/usr/local/bin/authchanger -reset -Okta -DefaultJCRight -preAuth JamfConnectLogin:RunScript,privileged JamfConnectLogin:Notify
+
+#/usr/local/bin/authchanger -reset -Okta -preAuth JamfConnectLogin:RunScript,privileged
+
+# BASIC VERSION: Just Okta Login.  No scripting or EULA.
+# /usr/local/bin/authchanger -reset -Okta
+
+
+# Use the killall command to make sure that a slow network connection will kill the standard macOS login window after the pkg has fully installed.
+/usr/bin/killall -9 loginwindow
+
+exit 0      ## Success
+exit 1      ## Fail
